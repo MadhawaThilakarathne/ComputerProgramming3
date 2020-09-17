58d779502a0b704a3b325b3ab06840ff0929f985
MadhawaThilakarathne <cscuser@L01-HP27.cssamba.jfn.ac.lk>
2020-09-18 04:36:32 +0530

    Added file

---
+++ CscExamples.java
@@ -0,0 +1,17 @@
+public class CscExamples
+{
+	int questionNo;
+	String category;
+	String text;
+	
+	public void setQuestionNo(){
+		
+	}
+	
+	public void setCategory(){
+		
+	}
+	public void setText(){
+		
+	}
+}
