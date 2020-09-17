770e5746cecda32a4da1c6f5b4a20d9e352d3fd9
MadhawaThilakarathne <cscuser@L01-HP27.cssamba.jfn.ac.lk>
2020-09-18 04:53:10 +0530

    add file

---
+++ CscExamples.java
@@ -0,0 +1,16 @@
+public class CscExamples{
+
+	private getQuestionNo;
+	private getCatogory;
+	private getText;
+
+
+	public void addQuestion(CscExample qu){
+	}
+	public void removeQuestion(int quId){
+	}
+	public void editQuestion(int quId){
+	}
+
+}
+
